8ca1f3ca12ffb0c5531db9526064b578ebec5cd0
yuq-1s <yuq406@gmain.com>
2018-10-18 19:10:02 +0800

    Lab4: copy answers.

--- labcodes/lab4/kern/process/proc.c
+++ labcodes/lab4/kern/process/proc.c
@@ -102,6 +102,10 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
+      memset(proc, 0, sizeof(struct proc_struct));
+      proc->state = PROC_UNINIT;
+      proc->pid = -1;
+      proc->cr3 = boot_cr3;
     }
     return proc;
 }
@@ -162,15 +166,25 @@ void
 proc_run(struct proc_struct *proc) {
     if (proc != current) {
         bool intr_flag;
-        struct proc_struct *prev = current, *next = proc;
-        local_intr_save(intr_flag);
+        /*struct proc_struct *prev = current, *next = proc;*/
+        local_intr_save(intr_flag); // Disable interrupt if enabled
         {
             current = proc;
-            load_esp0(next->kstack + KSTACKSIZE);
-            lcr3(next->cr3);
-            switch_to(&(prev->context), &(next->context));
+            load_esp0(proc->kstack + KSTACKSIZE);
+            lcr3(proc->cr3);
+            switch_to(&(current->context), &(proc->context));
         }
         local_intr_restore(intr_flag);
+        // XXX: Why rename current and proc here?
+        /*struct proc_struct *prev = current, *next = proc;*/
+        /*local_intr_save(intr_flag); // Disable interrupt if enabled*/
+        /*{*/
+            /*current = proc;*/
+            /*load_esp0(next->kstack + KSTACKSIZE);*/
+            /*lcr3(next->cr3);*/
+            /*switch_to(&(prev->context), &(next->context));*/
+        /*}*/
+        /*local_intr_restore(intr_flag);*/
     }
 }
 
@@ -296,6 +310,31 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
+    if ((proc = alloc_proc()) == NULL) {
+      goto fork_out;
+    }
+    proc->parent = current;
+    if (setup_kstack(proc) != 0) {
+      goto bad_fork_cleanup_proc;
+    }
+    if (copy_mm(clone_flags, proc) != 0) {
+      goto bad_fork_cleanup_kstack;
+    }
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+      proc->pid = get_pid();
+      hash_proc(proc);
+      list_add(&proc_list, &(proc->list_link));
+      nr_process++;
+    }
+    local_intr_restore(intr_flag);
+
+    wakeup_proc(proc);
+
+    ret = proc->pid;
 fork_out:
     return ret;
 
